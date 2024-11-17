c354a8b797d2528b7279120d3cc7d13761aca6c7
Grazvydas Z <grazvydas.cherry@gmail.com>
2024-11-17 10:34:52 +0200

    New: Setup scss directory.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,16 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint'],
+  css: ['~/assets/sass/style.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern',
+        },
+      },
+    },
+  },
   eslint: {
     config: {
       stylistic: {
