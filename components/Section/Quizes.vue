68c7c5d60172a8676d8f1b7eaa1e1b010c834855
Grandizas <grazvydaszilius2001@gmail.com>
2024-11-17 18:46:00 +0200

    New: Created a new quizes section.

---
+++ components/Section/Quizes.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="section-quizes">
+    <div class="section-quizes__card"></div>
+    <div class="section-quizes__card"></div>
+    <div class="section-quizes__card"></div>
+    <div class="section-quizes__card"></div>
+  </div>
+</template>
