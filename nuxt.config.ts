9f24faac5de1a684030905612e41dd0802c6dc01
Gražvydas <grazvydas.cherry@gmail.com>
2024-11-22 18:37:13 +0200

    Remove: animate.css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint'],
-  css: ['~/assets/sass/style.scss', 'animate.css/animate.min.css'],
+  css: ['~/assets/sass/style.scss'],
   vite: {
     css: {
       preprocessorOptions: {
